831fd3ad8e6129bf8c2a180ecc5f78ca295b8ad8
Martin de Keijzer <martin@spinninrecords.nl>
2019-08-19 16:13:14 +0200

    Add initial bundle setup

---
+++ Controller/CreateController.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+/**
+ * media-maestro-bundle - All rights reserved
+ */
+namespace Martin1982\MediaMaestroBundle\Controller;
+
+/**
+ * Class CreateController
+ */
+class CreateController
+{
+
+}
---
+++ MediaMaestroBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Martin1982\MediaMaestroBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MediaMaestroBundle
+ */
+class MediaMaestroBundle extends Bundle
+{
+}
